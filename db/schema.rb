e3f75663f339210543b4a49be316cd9c7ad438b5
Youngblood, Cameron <cameron.youngblood@gmail.com>
2023-02-23 23:07:41 -0700

    create Cart model and current_cart concern

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_042645) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_055634) do
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
