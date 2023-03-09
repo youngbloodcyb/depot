8f6520f25f8af1df9d2572a2e7bf777fc368d7d0
Youngblood, Cameron <cameron.youngblood@gmail.com>
2023-03-08 21:53:20 -0700

    begin adding checkout process

---
+++ app/models/order.rb
@@ -0,0 +1,12 @@
+class Order < ApplicationRecord
+    has_many :line_items, dependent: :destroy
+    
+    enum pay_type: {
+        "Check" => 0,
+        "Credit card" => 1,
+        "Purchase Order" => 2
+    }
+
+    validates :name, :address, :email, presence: true
+    validates :pay_type, inclusion: pay_types.keys
+end
