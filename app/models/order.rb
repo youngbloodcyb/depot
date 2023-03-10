a42b175fb46ba0a4fb892ce3533f25293c858ee3
Youngblood, Cameron <cameron.youngblood@gmail.com>
2023-03-09 22:12:16 -0700

    finish checkout form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ApplicationRecord
+    def add_line_items_from_cart(cart)
+        cart.line_items.each do |item|
+            item.cart_id = nil
+            line_items << item
+        end
+    end
+
     has_many :line_items, dependent: :destroy
     
     enum pay_type: {
